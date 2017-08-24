e67cb5f2dfda717c38708bda12ebb7386437dbb6
PC\James.Matthews <james.matthews@perfectchannel.com>
2017-08-24 16:11:48 +0100

    # updated

--- active_directory/AD_Add_Users_to_AD_Groups.ps1
+++ active_directory/AD_Add_Users_to_AD_Groups.ps1
@@ -1,13 +1,53 @@
-﻿# Script to ADD Active Directory Users to Active Directory Groups
-# Usage:
-# When defining users names, it needs to be there {SamName} you can get this form ADSIedit
-# When defining groups, you can use the {Group name (pre-Windows 2000)}
-# Remember to added Users with spaces in there {SamName} within double quotes " "
-# For Multiple Users, should be split with a comma e.g. "John Smith","John Brown"
+﻿<#
+.description: Script to ADD Active Directory Users to Active Directory Groups
 
-$Users  = "John Smith","Bob Smith"
-$Groups1 = "Local Admins (SQL Servers Admins)"
-$Groups2 = "Local Admins (Windows Admins)"
-Import-module ActiveDirectory  
-Add-ADGroupMember -Identity $Groups1 -Members $Users
-Add-ADGroupMember -Identity $Groups2 -Members $Users
+
+.usage:
+    When defining users names, it needs to be there {SamName} you can get this form ADSIedit
+    When defining groups, you can use the {Group name (pre-Windows 2000)}
+    Remember to added Users with spaces in there {SamName} within double quotes " "
+    For Multiple Users, should be split with a comma e.g. "John Smith","John Brown"
+
+
+############################################################################################################################
+Comments:
+
+Additional work:
+
+Adding the following:
+    Powershell Pester - Unit Testing
+        Find-Module -Name pester
+        install-module -Name pester
+    Powershell Script Analyzer - Code Analysis
+        Find-Module -Name PSScriptAnalyzer  (Powershell Gallery)
+        Install-Module -Name PSScriptAnalyzer
+        Invoke-ScriptAnalyzer -Path
+#>
+
+# Variables
+$Users  = "James Matthews"
+$Group = "DevOps"
+
+
+# Module Import
+Import-module ActiveDirectory 
+
+if (! ( get-ADGroupMember -Identity $group | where-object { $_.name -eq $Users } ) ) {
+    
+        Add-ADGroupMember -Identity $Group -Members $Users
+
+    }
+
+
+## Adding Pester Unit Tests
+Describe 'Active Directory' {
+
+    it 'Does Group Exists' {
+       (get-ADGroup -filter * | where-object { $_.name -eq $group } ) | Should Be $true
+    }
+
+    it 'Does User Part of Group' {
+        ( get-ADGroupMember -Identity $group | where-object { $_.name -eq $Users } ) | Should Be $true 
+    }
+    
+}
