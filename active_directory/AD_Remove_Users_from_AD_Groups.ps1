da2db08bc8ff98b1561d778ee833831d9ca6cae6
PC\James.Matthews <james.matthews@perfectchannel.com>
2017-08-24 16:20:48 +0100

    # add pester test

--- active_directory/AD_Remove_Users_from_AD_Groups.ps1
+++ active_directory/AD_Remove_Users_from_AD_Groups.ps1
@@ -1,13 +1,51 @@
-﻿# Script to REMOVE Active Directory Users FROM Active Directory Groups
-# Usage:
-# When defining users names, it needs to be there {SamName} you can get this form ADSIedit
-# When defining groups, you can use the {Group name (pre-Windows 2000)}
-# Remember to added Users with spaces in there {SamName} within double quotes " "
-# For Multiple Users, should be split with a comma e.g. "John Smith","John Brown"
+﻿<#
+.description: to REMOVE Active Directory Users FROM Active Directory Groups
 
-$Users  = "John Smith","Bob Smith"
-$Groups1 = "Local Admins (SQL Servers Admins)"
-$Groups2 = "Local Admins (Windows Admins)"
+
+.usage:
+    When defining users names, it needs to be there {SamName} you can get this form ADSIedit
+    When defining groups, you can use the {Group name (pre-Windows 2000)}
+    Remember to added Users with spaces in there {SamName} within double quotes " "
+    For Multiple Users, should be split with a comma e.g. "John Smith","John Brown"
+
+
+############################################################################################################################
+Comments:
+
+.add the following:
+    Powershell Pester - Unit Testing
+        Find-Module -Name pester
+        install-module -Name pester
+    Powershell Script Analyzer - Code Analysis
+        Find-Module -Name PSScriptAnalyzer  (Powershell Gallery)
+        Install-Module -Name PSScriptAnalyzer
+        Invoke-ScriptAnalyzer -Path
+#>
+
+
+# Variables
+$Users  = "James Matthews"
+$Group = "DevOps"
+
 Import-module ActiveDirectory  
-Remove-ADGroupMember -Identity $Groups1 -Members $Users -Confirm:$false
-Remove-ADGroupMember -Identity $Groups2 -Members $Users -Confirm:$false
+Remove-ADGroupMember -Identity $Group -Members $Users -Confirm:$false
+
+
+# Module Import
+Import-module ActiveDirectory 
+
+if ( $name = get-ADGroupMember -Identity $group | where-object { $_.name -eq $Users } )  {
+    
+        Remove-ADGroupMember -Identity $Group  -Members $name.SamAccountName -Confirm:$false
+
+    }
+
+
+## Adding Pester Unit Tests
+Describe 'Active Directory' {
+
+    it 'User Part of Group' {
+        ( get-ADGroupMember -Identity $group | where-object { $_.name -eq $Users } ) | Should Be $false
+    }
+    
+}
