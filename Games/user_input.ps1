d6322022aa426b5ff9f74fae9673a9a5eb4d7136
PC\James.Matthews <james.matthews@perfectchannel.com>
2017-09-05 10:06:18 +0100

    ## easy challenge - completed

---
+++ Games/user_input.ps1
@@ -0,0 +1,16 @@
+﻿<#
+.scenario: 
+
+    create a program that will ask the users name, age, and reddit username. have it tell them the information back, in the format:
+    your name is (blank), you are (blank) years old, and your username is (blank)
+    for extra credit, have the program log this information in a file to be accessed later.
+
+.link: https://www.reddit.com/r/dailyprogrammer/comments/pih8x/easy_challenge_1/
+
+#>
+
+$name = read-host "Enter your Full Name"
+$age = read-host "Enter your Age"
+$user = read-host "Enter your Username"
+
+Write-Output "Your name is $name, you are $age years old, and your username is $user" >> "C:\temp\task.log "
