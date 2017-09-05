a305274d7c591d3c6c54d5be8421740879195608
PC\James.Matthews <james.matthews@perfectchannel.com>
2017-09-05 16:50:53 +0100

    # another challenge - completed

---
+++ Games/day_of_week.ps1
@@ -0,0 +1,9 @@
+﻿<#
+.scenario: 
+
+   The program should take three arguments. The first will be a day, the second will be month, and the third will be year. 
+   Then, your program should compute the day of the week that date will fall on.
+
+.links - hhttps://www.reddit.com/r/dailyprogrammer/comments/pwons/2192012_challenge_11_easy/
+
+#>
---
+++ Games/input_sort_numbers.ps1
@@ -0,0 +1,18 @@
+﻿<#
+.scenario: 
+
+    write a program that will allow the user to input digits, and arrange them in numerical order.
+    for extra credit, have it also arrange strings in alphabetical order
+
+.links - https://www.reddit.com/r/dailyprogrammer/comments/pu1rf/2172012_challenge_9_easy/
+
+#>
+
+$numbers = @()
+
+[ValidatePattern("\d+")]$numbers = (Read-Host "Input any number (separate with comma)").split(',') | ForEach-Object {$_.trim()} | Sort-Object
+
+foreach ($output in $numbers)
+    {
+        $output 
+    }
